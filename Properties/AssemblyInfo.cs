2e95dde9ac3e04d72a91274e98e167e2727485c4
Håkan Edling <hakan@tidyui.com>
2012-08-14 15:56:40 +0200

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Piranha.Globalization")]
+[assembly: AssemblyDescription("Globalization library for the Piranha manager interface.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("piranhacms.org")]
+[assembly: AssemblyProduct("Piranha.Globalization")]
+[assembly: AssemblyCopyright("Copyright © piranhacms.org 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("735ee320-8cce-47b1-b04e-668f1d55f49b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
---
+++ README.md
@@ -0,0 +1,6 @@
+﻿Piranha.Globalization
+=====================
+This is the globalization project for the Piranha core that holds all of the 
+language resources for the manager interface. Fork this repository to create
+a new language translation or to correct any mistakes in any of the existing
+languages.
